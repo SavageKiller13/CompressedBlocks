3ff5f5a26437eb8b824b1931ebcd8935b047c57a
SavageKiller13 <sorenskittycat12@hotmail.com>
2018-07-13 22:50:10 -0600

    Edited Block Deconstructor

--- src/main/java/com/Savage_Killer13/CompressedBlocks/objects/blocks/machines/blockdeconstructor/BlockDeconstructorRecipes.java
+++ src/main/java/com/Savage_Killer13/CompressedBlocks/objects/blocks/machines/blockdeconstructor/BlockDeconstructorRecipes.java
@@ -5,8 +5,12 @@
  */
 package com.Savage_Killer13.CompressedBlocks.objects.blocks.machines.blockdeconstructor;
 
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
+import com.Savage_Killer13.CompressedBlocks.init.BlockInit;
+import com.google.common.collect.Maps;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 
 /**
@@ -15,18 +19,37 @@ import net.minecraft.item.ItemStack;
  */
 public class BlockDeconstructorRecipes {
     private static final BlockDeconstructorRecipes INSTANCE = new BlockDeconstructorRecipes();
-    private final Table<ItemStack, ItemStack, ItemStack> smeltingList = HashBasedTable.<ItemStack, ItemStack, ItemStack>create();
+    private final Map<ItemStack, ItemStack> deconstructingList = Maps.<ItemStack, ItemStack>newHashMap();
     
-    public BlockDeconstructorRecipes getInstance() {
+    public static BlockDeconstructorRecipes getInstance() {
         return INSTANCE;
     }
     
     private BlockDeconstructorRecipes() {
-        
+        addDeconstructRecipe(new ItemStack(BlockInit.DENSE_COBBLESTONE), new ItemStack(Blocks.COBBLESTONE, 8));
+        addDeconstructRecipe(new ItemStack(BlockInit.DENSE_DIRT), new ItemStack(Blocks.DIRT));
     }
     
     public void addDeconstructRecipe(ItemStack input, ItemStack result) {
         if(getDeconstructResult(input) != ItemStack.EMPTY) return;
-        this.smeltingList.put(input, result);
+        this.deconstructingList.put(input, result);
+    }
+    
+    public ItemStack getDeconstructResult(ItemStack input) {
+        for(Entry<ItemStack, ItemStack> entry : this.deconstructingList.entrySet()) {
+            if (this.compareItemStacks(input, entry.getKey())) {
+                return entry.getValue();
+            }
+        }
+        
+        return ItemStack.EMPTY;
+    }
+    
+    private boolean compareItemStacks(ItemStack stack1, ItemStack stack2) {
+        return stack2.getItem() == stack1.getItem() && (stack2.getMetadata() == 32767 || stack2.getMetadata() == stack1.getMetadata());
+    }
+    
+    public Map getDeconstructList() {
+        return this.deconstructingList;
     }
 }
